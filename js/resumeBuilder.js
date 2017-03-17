c011778e43d837a51ac1ab6fc40d929f22df196e
Aby Abraham Kalathivila <abyabrkal@gmail.com>
2017-03-17 23:04:22 +0400

    Progressing Udacity
    Lesson 3.5

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var name = "Aby Abraham";
+var formattedName = HTMLheaderName.replace('%data%', name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace('%data%', role);
+
+$('#header').append(formattedName);
+$('#header').append(formattedRole);
